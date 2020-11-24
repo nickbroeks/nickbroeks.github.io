b917db650bf6f7d9e3ce3a562ee888c955dbddbc
nickbroeks <nick.broeks@live.nl>
2020-11-24 02:34:58 +0100

    small bugfix

--- groepeen/sketch.js
+++ groepeen/sketch.js
@@ -44,8 +44,7 @@ function openInfo(node) {
         document.getElementsByClassName("candidatePic")[i].setAttribute("onClick", "");
     }
     openID = node.getAttribute("candID");
-    //document.getElementById("candInfo" + openID).hidden = false;
-    document.getElementById("candInfo"+ openID).style.display = "block";
+    document.getElementById("candInfo" + openID).hidden = false;
     document.getElementById("closeClick").hidden = false;
     
     setTimeout( () => {
@@ -67,7 +66,7 @@ function closeInfo(e) {
         document.getElementById("candInfo" + openID).style.opacity = 0;
         document.getElementById("closeClick").setAttribute("onClick", "");
         setTimeout( () => {
-            //document.getElementById("candInfo" + openID).hidden = true;
+            document.getElementById("candInfo" + openID).hidden = true;
             document.getElementById("closeClick").hidden = true;
             
         }, 1000)
