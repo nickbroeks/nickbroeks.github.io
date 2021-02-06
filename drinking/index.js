e384c9873a3e24e0bec8e842c407488ebac7c751
nickbroeks <nick.broeks@live.nl>
2021-02-06 22:00:43 +0100

    change drinking

--- drinking/index.js
+++ drinking/index.js
@@ -1,13 +1,28 @@
-const test = ["Nick", "Anne", "Cindy", "Sophie", "Niels"];
+let names = [];
 
 function press() {
-    let pers = test[Math.floor(Math.random() * test.length)];
+    let pers = names[Math.floor(Math.random() * names.length)];
     if (pers == "Inge") {
         document.getElementById("1").innerText = pers
         document.getElementById("2").innerText = 3 + Math.floor(Math.random() * 3);
     } else {
         document.getElementById("1").innerText = pers
-        document.getElementById("2").innerText = 3 + Math.floor(Math.random() * 3);
+        document.getElementById("2").innerText = 1 + Math.floor(Math.random() * 4);
     }
-    setTimeout(press, 4000);
+    setTimeout(press, 10000);
+}
+
+function submitName() {
+    let newName = document.querySelector("#config-tab-input-name").value;
+    names.push(newName);
+    let obj = document.createElement("span");
+    obj.innerText = newName;
+    document.querySelector("#config-tab-names").appendChild(obj);
+    document.querySelector("#config-tab-names").appendChild(document.createElement("br"));
+}
+
+function startGame() {
+    document.querySelector('#config-tab').style.visibility = "hidden";
+    document.querySelector('#game-tab').style.visibility = "visible";
+    press();
 }
