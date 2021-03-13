b3cb3d12caf113b98f2f764471017461bd60dccc
nickbroeks <nick.broeks@live.nl>
2021-03-13 21:45:06 +0100

    woopwoop

--- drinking/index.js
+++ drinking/index.js
@@ -2,14 +2,30 @@ let names = [];
 
 function press() {
     let pers = names[Math.floor(Math.random() * names.length)];
+    let number;
     if (pers == "Inge") {
         document.getElementById("1").innerText = pers
-        document.getElementById("2").innerText = 3 + Math.floor(Math.random() * 3);
+        number = 3 + Math.floor(Math.random() * 3);
     } else {
         document.getElementById("1").innerText = pers
-        document.getElementById("2").innerText = 1 + Math.floor(Math.random() * 4);
+        number = 1 + Math.floor(Math.random() * 4);
     }
-    setTimeout(press, 10000);
+    document.getElementById("2").innerText = number
+    setTimeout(press, 120000);
+    document.body.classList.add("Red");
+    setTimeout(()=> {document.body.classList.remove("Red")}, 3000)
+    setTimeout(()=> {document.body.classList.add("Red")}, 4000)
+    setTimeout(()=> {document.body.classList.remove("Red")}, 5000)
+    setTimeout(()=> {document.body.classList.add("Red")}, 6000)
+    setTimeout(()=> {document.body.classList.remove("Red")}, 7000)
+    setTimeout(()=> {document.body.classList.add("Red")}, 8000)
+    setTimeout(()=> {document.body.classList.remove("Red")}, 9000)
+
+    let utter = new SpeechSynthesisUtterance()
+    utter.lang = 'nl';
+    utter.text = `Hallo.${pers} drinkt ${number} keer, ${number} keer`;
+    window.speechSynthesis.speak(utter);
+    
 }
 
 function submitName() {
